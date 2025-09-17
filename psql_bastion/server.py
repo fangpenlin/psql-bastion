69b45850c277e59e74ffd626b6ea61f3cea3ed4e
Fang-Pen Lin <hello@fangpenlin.com>
2025-09-16 18:15:51 -0700

    Fix stuff

--- psql_bastion/server.py
+++ psql_bastion/server.py
@@ -7,13 +7,29 @@ from psycopg.types import TypeInfo
 logger = logging.getLogger(__name__)
 
 PG_TO_ARROW = {
+    'int2': pa.int16(),
     'int4': pa.int32(),
-    'text': pa.string(),
+    'int8': pa.int64(),
+    'float4': pa.float32(),
     'float8': pa.float64(),
+    'text': pa.string(),
+    'varchar': pa.string(),
+    'char': pa.string(),
     'bool': pa.bool_(),
-    'timestamp': pa.timestamp('ns')
+    'timestamp': pa.timestamp('ns'),
+    'date': pa.date32(),
 }
 DB_URL = 'postgres://infisical:infisical@localhost:5432/infisical'
+
+def fetch_type_info_by_oid(conn, oid):
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT t.typname
+            FROM pg_type t
+            WHERE t.oid = %s
+        """, (oid,))
+        return cur.fetchone()[0]
+
 
 class Connection(riffq.BaseConnection):
     def handle_auth(self, user, password, host, database=None, callback=callable):
@@ -36,22 +52,34 @@ class Connection(riffq.BaseConnection):
 
     def _handle_query(self, sql, callback, **kwargs):
         logger.info("SQL sql=%s", sql)
-        with self.outgoing_conn.cursor() as cursor:
-            cursor.execute(sql)
-            schema = pa.schema([
-                (desc[0], PG_TO_ARROW.get(TypeInfo.fetch(self.outgoing_conn, desc.type_code).name, pa.string()))
-                for desc in cursor.description
-            ])
-            # TODO: well... if this is huge, ideally we want to stream the result back to the client
-            rows = cursor.fetchall()
-            data_columns = list(zip(*rows))
-            # Convert data to PyArrow arrays
-            arrays = [
-                pa.array(data_columns[i], type=field.type)
-                for i, field in enumerate(schema)
-            ]
-            record_batch = pa.RecordBatch.from_arrays(arrays, schema=schema)
-            self.send_reader(record_batch, callback)
+        try:
+            with self.outgoing_conn.cursor() as cursor:
+                cursor.execute(sql)
+                schema = pa.schema([
+                    # TODO: this fetch_type_info_by_oid is very slow, maybe we should cache it or query all at once
+                    #       instead?
+                    (desc[0], PG_TO_ARROW.get(fetch_type_info_by_oid(self.outgoing_conn, desc.type_code), pa.string()))
+                    for desc in cursor.description
+                ])
+                # TODO: well... if this is huge, ideally we want to stream the result back to the client
+                rows = cursor.fetchall()
+                data_columns = list(zip(*rows))
+                # Convert data to PyArrow arrays
+                arrays = [
+                    pa.array(data_columns[i], type=field.type)
+                    for i, field in enumerate(schema)
+                ]
+                record_batch = pa.RecordBatch.from_arrays(arrays, schema=schema)
+                self.send_reader(record_batch, callback)
+        except Exception as exc:
+            logger.error("Failed to execute SQL", exc_info=True)
+            batch = self.arrow_batch(
+                # TODO: well, this is from the example. revealing details exc info may not be a good idea from the
+                #       perspective of security
+                [pa.array(["ERROR"]), pa.array([str(exc)])],
+                ["error", "message"],
+            )
+            self.send_reader(batch, callback)
 
     def handle_query(self, sql, callback=callable, **kwargs):
         self.executor.submit(self._handle_query, sql, callback, **kwargs)
